226da2481dee51705ac2a41ffd5c13a9aa2a4378
Jorge Perez <jorgeimon100@gmail.com>
2026-05-04 11:42:05 +0200

    smoke

--- smoke/smoke.js
+++ smoke/smoke.js
@@ -1,71 +1,72 @@
-
-
 const num_of_particles = 100;
-const min_particle_size = 0.2
-const min_speed_x = 1
-const min_speed_y = 1
+const min_particle_size = 0.2;
+const min_speed_x = 1;
+const min_speed_y = 1;
 
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('smoke_canvas');
 const ctx = canvas.getContext('2d');
-//define the color that we want to use four our drawings
+
+// define the color
 ctx.fillStyle = 'rgb(47, 55, 109)';
-//prepare to draw a path
 ctx.beginPath();
-//draw a circle with the arc method
 ctx.arc(100, 100, 50, 0, Math.PI * 2);
-//fill the object that we just created
 ctx.fill();
 
-const smoke = new Smoke();
-console.log(smoke);
-window.addEventListener('mousemove', (e)=>{});
-console.log(e.clientX);
-console.log(e.clientY);
-console.log(e);
+class Smoke {
+    constructor() {
+        this.particles = [];
+    }
 
-// createng new particle
-for (let index = 0; index < num_of_particles; index++) {                              
-    const particle = new Particle(e.clientX, e.clientY);
-    smoke.add(particle);
-    console.log(smoke.particles);
+    add(particle) {
+        this.particles.push(particle);
+    }
 
-}
-new Particle(e.clientX, e.clientY);
-
-
-
-class Smoke{
-    constructor(){
-    this.Particle = [];
-
+    animate() {
+        for (let index = 0; index < this.particles.length; index++) {
+            //update the sates of particle and draw it to the screen
+            this.particles[index].update();
+            this.particles[index].draw();
+        }
+        requestAnimationFrame(this.animate);
 
 }
-
-add(particle){
-    this.particle
-
-
 }
-
-}
-class Particle{
-
-    constructor(x, y){
-        this.size = Math.random() * 2 + min_particle_size ;
-        this.color =  'rgb(47, 55, 109)';
+class Particle {
+    constructor(x, y) {
+        this.size = Math.random() * 2 + min_particle_size;
+        this.color = 'rgb(47, 55, 109)';
         this.speedx = Math.random() * 2 + min_speed_x;
         this.speedy = Math.random() * 2 + min_speed_y;
         this.x = x;
         this.y = y;
-
     }
 
-    update(){
+    update() {
         this.size -= 1;
-        // this.size = this.size - 1;
+    }
 
-
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
 }
 
-}
+const smoke = new Smoke();
+console.log(smoke);
+
+window.addEventListener('mousemove', (e) => {
+    console.log(e.clientX);
+    console.log(e.clientY);
+    console.log(e);
+
+    for (let index = 0; index < num_of_particles; index++) {
+        const particle = new Particle(e.clientX, e.clientY);
+        smoke.add(particle);
+        console.log(smoke.particles);
+    }
+});
+
+smoke.animate();
 
